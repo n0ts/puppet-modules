5933e418d3e42ebae9f9cf6acd6718235916efd7
Naoya NAKAZAWA <naoya.n@gmail.com>
2009-12-10 18:06:17 +0900

    Use template

--- memcached/manifests/init.pp
+++ memcached/manifests/init.pp
@@ -2,14 +2,28 @@
 # memcached module
 #
 
+
 class memcached {
+    if $memcached_port == "" {
+        $memcached_port = "11211"
+    }
+    if $memcached_cachesize == "" {
+        $memcached_cachesize = "512"
+    }
+    if $memcached_maxconn == "" {
+        $memcached_maxconn = "1024"
+    }
+    if $memcached_options == "" {
+        $memcached_options = ""
+    }
+
     file { "/etc/sysconfig/memcached":
-        source => "puppet:///memcached/etc/sysconfig/memcached",
+        content => template("memcached/etc/sysconfig/memcached"),
     }
 
     service { "memcached":
         subscribe => File["/etc/sysconfig/memcached"],
-        require => Package["memcached"],
+        require   => Package["memcached"],
     }
 
     package { "memcached": }
